6d41c09189b80c8aa959977a858a605adc9a8d4e
jeyoonee <jae4419@naver.com>
2025-03-14 21:30:49 +0900

    Remove unnecessary files from tracking

---
+++ src/AppProfile.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import Avatar from "./components/Avatar";
+import Profile from "./components/Profile";
+
+function AppProfile() {
+  return (
+    <>
+      <Avatar
+        image="https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?q=80&w=2120&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        isNew={true}
+      />
+      <Profile
+        image="https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?q=80&w=2120&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        name="Jeyoon Jeong"
+        title="Frontend Engineer"
+        isNew={true}
+      />
+      <Profile
+        image="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        name="Anna Young"
+        title="Backend Engineer"
+      />
+      <Profile
+        image="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        name="Bob yu"
+        title="Project Manager"
+      />
+    </>
+  );
+}
+
+export default AppProfile;
