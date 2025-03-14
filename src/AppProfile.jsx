11b067f61acdf0d309abab2c52284afb32b2bf32
jeyoonee <jae4419@naver.com>
2025-03-14 21:43:03 +0900

    feat(button): add alert message on click

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -3,8 +3,13 @@ import Avatar from "./components/Avatar";
 import Profile from "./components/Profile";
 
 function AppProfile() {
+  const handleClick = (event) => {
+    console.log(event);
+    alert("버튼을 클릭!");
+  };
   return (
     <>
+      <button onClick={handleClick}>버튼</button>
       <Avatar
         image="https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?q=80&w=2120&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         isNew={true}
