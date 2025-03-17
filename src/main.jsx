f2082596787f6972f5474d8e01c2ac2ab07a141b
jeyoonee <jae4419@naver.com>
2025-03-17 23:59:09 +0900

    feat(pointer): complete pointer component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import AppProfile from "./AppProfile.jsx";
-import AppProducts from "./AppProducts";
+import AppXY from "./AppXY";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppProducts />
+    <AppXY />
   </StrictMode>
 );
