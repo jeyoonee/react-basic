10a393ba42e7a79532182f4d6bfd12dfcfc26c3e
jeyoonee <jae4419@naver.com>
2025-03-17 20:14:12 +0900

    feat(counter): add counter component

---
+++ src/AppCounter.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Counter from "./components/Counter";
+import { useState } from "react";
+
+function AppCounter() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  return (
+    <div className="container">
+      <div className="banner">
+        Total Count: {count} {count > 10 ? "🔥" : "❄"}
+      </div>
+      <div className="counters">
+        <Counter total={count} onClick={handleClick} />
+        <Counter total={count} onClick={handleClick} />
+      </div>
+    </div>
+  );
+}
+
+export default AppCounter;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppProfile from "./AppProfile.jsx";
+import AppCounter from "./AppCounter";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppProfile />
+    <AppCounter />
   </StrictMode>
 );
