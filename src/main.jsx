c66d830410c3b3c23f8260e963e0c58f05aede3d
jeyoonee <jae4419@naver.com>
2025-03-17 22:26:57 +0900

    feat(products): add products component for learning useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppProfile from "./AppProfile.jsx";
-import AppCounter from "./AppCounter";
+import AppProducts from "./AppProducts";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppCounter />
+    <AppProducts />
   </StrictMode>
 );
