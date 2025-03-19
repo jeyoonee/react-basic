8b4fb9e9bdc8479337dc7a01fcd9081f703d3e09
jeyoonee <jae4419@naver.com>
2025-03-19 14:28:56 +0900

    feat: add AppTheme using useContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import AppCard from "./AppCard";
+import AppTheme from "./AppTheme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppCard />
+    <AppTheme />
   </StrictMode>
 );
