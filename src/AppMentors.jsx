b11f70ae6c0ed5c2a4980bb33e26972e22705bb6
jeyoonee <jae4419@naver.com>
2025-03-19 15:45:58 +0900

    refactor: replace with useMemo and useCallback to improve performance

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,5 +1,70 @@
-import React, { useReducer } from "react";
+import React, { memo, useCallback, useMemo, useReducer } from "react";
 import personReducer from "./reducer/person-reducer";
+
+export default function AppMentorsButton() {
+  const [person, dispatch] = useReducer(personReducer, initialPerson);
+
+  const handleUpdate = useCallback(() => {
+    const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
+    const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
+    dispatch({ type: "updated", prev, current });
+  }, []);
+
+  const handleAdd = useCallback(() => {
+    const name = prompt(`멘토의 이름은?`);
+    const title = prompt(`멘토의 직함은?`);
+    dispatch({ type: "added", name, title });
+  }, []);
+
+  const handleDelete = useCallback(() => {
+    const name = prompt(`누구를 삭제하고 싶은가요?`);
+    dispatch({ type: "deleted", name });
+  }, []);
+
+  return (
+    <div>
+      <h1>
+        {person.name}는 {person.title}
+      </h1>
+      <p>{person.name}의 멘토는:</p>
+      <ul>
+        {person.mentors.map((mentor, index) => (
+          <li key={index}>
+            {mentor.name} ({mentor.title})
+          </li>
+        ))}
+      </ul>
+      <Button text="멘토 이름 바꾸기" onClick={handleUpdate} />
+      <Button text="삭제하기" onClick={handleDelete} />
+      <Button text="멘토 추가하기" onClick={handleAdd} />
+    </div>
+  );
+}
+
+const Button = memo(({ text, onClick }) => {
+  console.log("Button", text, "re-rendering 😜");
+  const result = useMemo(() => calculateSomething(), []);
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        borderRadius: "20px",
+        margin: "0.4rem",
+      }}
+    >
+      {`${text} ${result}`}
+    </button>
+  );
+});
+
+function calculateSomething() {
+  for (let i = 0; i < 10000; i++) {
+    console.log("😆");
+  }
+  return 10;
+}
 
 const initialPerson = {
   name: "엘리",
@@ -15,48 +80,3 @@ const initialPerson = {
     },
   ],
 };
-
-export default function AppMentors() {
-  const [person, dispatch] = useReducer(personReducer, initialPerson);
-
-  const handleUpdate = () => {
-    const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
-    const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-
-    dispatch({ type: "update", prev, current });
-  };
-
-  const handleAdd = () => {
-    const name = prompt(`추가하고 싶은 멘토의 이름은?`);
-    const title = prompt(`어떤 직무인가요?`);
-
-    dispatch({ type: "added", name, title });
-  };
-
-  const handleDelete = () => {
-    const name = prompt(`삭제할 멘토의 이름을 입력하세요`);
-
-    dispatch({ type: "deleted", name });
-  };
-
-  return (
-    <div>
-      <h1>
-        {person.name}는 {person.title}
-      </h1>
-      <p>{person.name}의 멘토는:</p>
-
-      <ul>
-        {person.mentors.map((mentor, index) => (
-          <li key={index}>
-            {mentor.name} ({mentor.title})
-          </li>
-        ))}
-      </ul>
-
-      <button onClick={handleUpdate}>멘토의 이름을 바꾸기</button>
-      <button onClick={handleAdd}>멘토 추가하기</button>
-      <button onClick={handleDelete}>멘토 삭제하기</button>
-    </div>
-  );
-}
